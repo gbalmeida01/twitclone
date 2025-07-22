b3111573944598774b64b4895579a72ba85251eb
Gabriel Almeida <gjalmeida0309@gmail.com>
2025-07-21 23:53:03 -0300

    Remove application.properties from version control

---
+++ src/main/java/com/twitclone/twitclone/TwitCloneApplication.java
@@ -0,0 +1,13 @@
+package com.twitclone.twitclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twitclone/twitclone/TwitCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitclone.twitclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
